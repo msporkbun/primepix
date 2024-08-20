19481a4448eb5afe3c964dfda2448dae187325fe
Patty <pattamon.nakvi@gmail.com>
2024-08-21 06:31:29 +1000

    updated movies and movie page

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,56 +1,121 @@
 @extends('layouts.main')
 
 @section('content')
-    <div class="container mx-auto px-4 pt-16">
-        <div class="popular-movies">
-            <div class="movie-info
-                border-b border-gray-800">
-                <div class="container mx-auto px-4 py-16 flex flex-col md:flex-row">
-                    <img src="/img/parasite.jpg" alt="parasite" class="w-64 lg:w-96">
-                    <div class="md:ml-24">
-                        <h2 class="text-4xl font-semibold">Parasite (2019)</h2>
-                        <div class="flex flex-wrap items-center text-gray-400 text-sm">
-                            <span>
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" class="fill-current w-4 text-yellow-500">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
-                                </svg>
-                            </span>
-                            <span class="ml-1">85%</span>
-                            <span class="mx-2">|</span>
-                            <span>Feb 20, 2020</span>
-                            <span class="mx-2">|</span>
-                            <span>Action, Thriller</span>
+    <div class="movie-info border-b border-gray-800">
+        <div class="container mx-auto px-4 py-16 flex flex-col md:flex-row">
+            <div class="flex-none">
+                <img src="/img/parasite.jpg" alt="poster" class="sm:w-64 lg:w-96">
+            </div>
+            <div class="md:ml-24">
+                <h2 class="text-4xl font-semibold">Parasite (2019)</h2>
+                <div class="flex flex-wrap items-center text-gray-400 text-sm">
+                    <span>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" class="fill-current w-4 text-yellow-500">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
+                        </svg>
+                    </span>
+                    <span class="ml-1">85%</span>
+                    <span class="mx-2">|</span>
+                    <span>Feb 20, 2020</span>
+                    <span class="mx-2">|</span>
+                    <span>Action, Thriller</span>
+                </div>
+                <p class="text-gray-300 mt-8">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget nunc. Nullam auctor, nisl nec ultricies ultricies, nunc nunc ultricies nunc, nec ultricies nunc nunc nec nunc. Nullam auctor, nisl nec ultricies ultricies, nunc nunc ultricies nunc, nec ultricies nunc nunc nec nunc.
+                </p>
+                <div class="mt-12">
+                    <h4 class="text-white
+                        font-semibold">Featured Cast</h4>
+                    <div class="flex mt-4">
+                        <div>
+                            <div>Bong Joon-ho</div>
+                            <div class="text-sm text-gray-400">Screenplay, Director, Story</div>
                         </div>
-                        <p class="text-gray-300 mt-8">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget nunc. Nullam auctor, nisl nec ultricies ultricies, nunc nunc ultricies nunc, nec ultricies nunc nunc nec nunc. Nullam auctor, nisl nec ultricies ultricies, nunc nunc ultricies nunc, nec ultricies nunc nunc nec nunc.
-                        </p>
-                        <div class="mt-12">
-                            <h4 class="text-white
-                                font-semibold">Featured Cast</h4>
-                            <div class="flex mt-4">
-                                <div>
-                                    <div>Bong Joon-ho</div>
-                                    <div class="text-sm text-gray-400">Screenplay, Director, Story</div>
-                                </div>
-                                <div class="ml-8">
-                                    <div>Choi Woo-shik</div>
-                                    <div class="text-sm text-gray-400">Ki-taek</div>
-                                </div>
-                            </div>
+                        <div class="ml-8">
+                            <div>Han Jin-won</div>
+                            <div class="text-sm text-gray-400">Screenplay</div>
                         </div>
-                        <div class="mt-12">
-                            <button class="flex items-center bg-orange-500 text-gray-900 rounded font-semibold px-5 py-4 hover:bg-orange-600 transition ease-in-out duration-150">
-                                <span>
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" class="fill-current w-6">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M9 5l7 7-7 7" />
-                                    </svg>
-                                </span>
-                                <span class="ml-2">Play Trailer</span>
-                            </button>
-                        </div>
+                    </div>
+                </div>
+                <div class="mt-12">
+                    <button class="flex items-center bg-orange-500 text-gray-900 rounded font-semibold px-5 py-4 hover:bg-orange-600 transition ease-in-out duration-150">
+                        <span>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.347a1.125 1.125 0 0 1 0 1.972l-11.54 6.347a1.125 1.125 0 0 1-1.667-.986V5.653Z" />
+                            </svg>
+                        </span>
+                        <span class="ml-2">Play Trailer</span>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div> <!-- end movie-info -->
+
+    <div class="movie-cast border-b border-gray-800">
+        <div class="container mx-auto px-4 py-16">
+            <h2 class="text-4xl font-semibold">Cast</h2>
+            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8">
+                <!-- card:component -->
+                <div class="mt-8">
+                    <a href="#">
+                        <img src="/img/parasite.jpg" alt="parasite" class="hover:opacity-75 transition ease-in-out duration-200">
+                    </a>
+                    <div class="mt-2">
+                        <a href="#" class="text-lg mt-2 hover:text-gray:300">Real Name</a>
+                        <div class="text-sm text-gray-400">Character Name</div>
+                    </div>
+                </div>
+                <!-- card:component -->
+                <div class="mt-8">
+                    <a href="#">
+                        <img src="/img/parasite.jpg" alt="parasite" class="hover:opacity-75 transition ease-in-out duration-200">
+                    </a>
+                    <div class="mt-2">
+                        <a href="#" class="text-lg mt-2 hover:text-gray:300">Real Name</a>
+                        <div class="text-sm text-gray-400">Character Name</div>
+                    </div>
+                </div>
+                <!-- card:component -->
+                <div class="mt-8">
+                    <a href="#">
+                        <img src="/img/parasite.jpg" alt="parasite" class="hover:opacity-75 transition ease-in-out duration-200">
+                    </a>
+                    <div class="mt-2">
+                        <a href="#" class="text-lg mt-2 hover:text-gray:300">Real Name</a>
+                        <div class="text-sm text-gray-400">Character Name</div>
+                    </div>
+                </div>
+                <!-- card:component -->
+                <div class="mt-8">
+                    <a href="#">
+                        <img src="/img/parasite.jpg" alt="parasite" class="hover:opacity-75 transition ease-in-out duration-200">
+                    </a>
+                    <div class="mt-2">
+                        <a href="#" class="text-lg mt-2 hover:text-gray:300">Real Name</a>
+                        <div class="text-sm text-gray-400">Character Name</div>
+                    </div>
+                </div>
+                <!-- card:component -->
+                <div class="mt-8">
+                    <a href="#">
+                        <img src="/img/parasite.jpg" alt="parasite" class="hover:opacity-75 transition ease-in-out duration-200">
+                    </a>
+                    <div class="mt-2">
+                        <a href="#" class="text-lg mt-2 hover:text-gray:300">Real Name</a>
+                        <div class="text-sm text-gray-400">Character Name</div>
+                    </div>
+                </div>
+                <!-- card:component -->
+                <div class="mt-8">
+                    <a href="#">
+                        <img src="/img/parasite.jpg" alt="parasite" class="hover:opacity-75 transition ease-in-out duration-200">
+                    </a>
+                    <div class="mt-2">
+                        <a href="#" class="text-lg mt-2 hover:text-gray:300">Real Name</a>
+                        <div class="text-sm text-gray-400">Character Name</div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
+    </div> <!-- end movie-cast -->
 @endsection
