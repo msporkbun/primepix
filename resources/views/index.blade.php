7cacbfbe29922c4a1ac5300f4b9890a69d9fa3ff
Patty <pattamon.nakvi@gmail.com>
2024-08-21 23:34:26 +1000

    updated movies page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,20 +2,16 @@
 
 @section('content')
     <div class="container mx-auto px-4 pt-16">
-        <div class="popular-movies">
-            <h2 class="uppercase tracking-wider text-orange-500 text-lg font-semibold">Popular Movies</h2>
-            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8">
-                <!-- card:component -->
-                @foreach ($popularMovies as $movie)
-                    <x-movie-card :movie="$movie" :genres="$genres"/>
-                @endforeach
-            </div>
-        </div><!-- end popular-movies -->
+        <h2 class="uppercase tracking-wider text-orange-500 text-lg font-semibold">Popular Movies</h2>
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8">
+            @foreach ($popularMovies as $movie)
+                <x-movie-card :movie="$movie" :genres="$genres"/>
+            @endforeach
+        </div>
 
         <div class="now-playing-movies py-24">
             <h2 class="uppercase tracking-wider text-orange-500 text-lg font-semibold">Now Playing Movies</h2>
             <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8">
-                <!-- card:component -->
                 @foreach ($nowPlayingMovies as $movie)
                     <x-movie-card :movie="$movie" :genres="$genres"/>
                 @endforeach
