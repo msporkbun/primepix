76fdf751be7b85405bdbf6c1a0e4539291b0e744
Patty <pattamon.nakvi@gmail.com>
2024-08-22 00:53:52 +1000

    updated navbar links

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -12,11 +12,11 @@
             <ul class="flex flex-col md:flex-row items-center">
                 <!-- nav:logo -->
                 <li>
-                    <a href="#" class="hover:text-gray-300">PrimePix</a>
+                    <a href="{{ route('movies.index') }}" class="hover:text-gray-300">PrimePix</a>
                 </li>
                 <!-- nav:items -->
                 <li class="md:ml-16 mt-3 md:mt-0">
-                    <a href="#" class="hover:text-gray-300">Movies</a>
+                    <a href="{{ route('movies.index') }}" class="hover:text-gray-300">Movies</a>
                 </li>
                 <li class="md:ml-6 mt-3 md:mt-0">
                     <a href="#" class="hover:text-gray-300">TV Shows</a>
