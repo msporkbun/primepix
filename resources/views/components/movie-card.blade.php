e4f4560a62790867e3c3d73176572c07a0092b47
Patty <pattamon.nakvi@gmail.com>
2024-08-21 23:21:43 +1000

    updated MovieCard component

--- resources/views/components/movie-card.blade.php
+++ resources/views/components/movie-card.blade.php
@@ -1,3 +1,23 @@
-<div>
-    <!-- People find pleasure in different ways. I find it in keeping my mind clear. - Marcus Aurelius -->
+<div class="mt-8">
+    <a href="#">
+        <img src="{{ 'https://image.tmdb.org/t/p/w500/'.$movie['poster_path'] }}" alt="{{ $movie['title'] }} poster" class="hover:opacity-75 transition ease-in-out duration-200">
+    </a>
+    <div class="mt-2">
+        <a href="#" class="text-lg mt-2 hover:text-gray:300">{{ $movie['title'] }}</a>
+        <div class="flex items-center text-gray-400 text-sm mt-1">
+            <span>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" class="fill-current w-4 text-yellow-500">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
+                </svg>
+            </span>
+            <span class="ml-1">{{ round($movie['vote_average'] * 10).'%' }}</span>
+            <span class="mx-2">|</span>
+            <span>{{ \Carbon\Carbon::parse($movie['release_date'])->format('d M Y')}}</span>
+        </div>
+        <div class="text-gray-400 text-sm">
+            @foreach ($movie['genre_ids'] as $genre)
+                {{ $genres->get($genre) }}@if (!$loop->last), @endif
+            @endforeach
+        </div>
+    </div>
 </div>
